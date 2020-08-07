7dc1ce3b516082fc17a9ada131b4ca0bcc0fd3f7
Guilherme Constantinov <guilherme_constantinov@alunos.fho.edu.br>
2020-08-07 16:20:29 -0300

    objeto banco

--- class/UserDao.php
+++ class/UserDao.php
@@ -1,10 +1,13 @@
 <?php
     require_once 'Connect.php';
+    require_once 'User.php';
     class UserDao{
 
          public function create(User $user){
-             $sql = "INSERT INTO usuarios_sistemas(nome,login,senha,admin) values()";
+             $sql = "INSERT INTO usuarios_sistemas(nome,login,senha,admin) values(?,?)";
              $stmt = Connect::Connect()->prepare($sql);
+             $stmt->bindValue(1,$this->)
+
 
          }
 
