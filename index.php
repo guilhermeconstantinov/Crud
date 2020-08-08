4f92e6609a5c7d1feb7f7263bad40866f3a37570
Guilherme Constantinov <guilherme_constantinov@alunos.fho.edu.br>
2020-08-08 14:11:09 -0300

    index.html to index.php

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html lang="pt-br">
     <head>
         <title>Crud - Estagiando</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
     <body>
